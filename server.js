3e177be3e28afa2b811fe7598237bc68d94cb599
DhanashreeNirmale <dhanashreenirmale@gmail.com>
2026-04-30 11:10:50 +0530

    WDD ASS 10

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const ENV = process.env.NODE_ENV || "development";
+
+app.get("/", (req, res) => {
+  res.send("Application Deployed Successfully");
+});
+
+app.get("/status", (req, res) => {
+  res.json({
+    status: "running",
+    environment: ENV
+  });
+});
+
+app.get("/info", (req, res) => {
+  res.json({
+    name: "Express Deployment App",
+    version: "1.0.0",
+    author: "Your Name"
+  });
+});
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Route not found"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
